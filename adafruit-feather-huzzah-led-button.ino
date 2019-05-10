46e6eb3428369c2c26672cfdd7a80a7b55fe6be8
Flavio H C Mauro <flaviomauro@users.noreply.github.com>
2019-05-09 23:30:45 -0300

    Add files via upload

---
+++ adafruit-feather-huzzah-led-button.ino
@@ -0,0 +1,41 @@
+/* 
+ *******************************************************************************
+ * Description:
+ * Sample code to turn a led on and off when pressing and releasing a button.
+ * GitHub: https://github.com/flaviomauro/Adafruit_Feather_Huzzah_Led_Button
+ * YouTube: https://youtu.be/O6CJU-kRd-k
+ *******************************************************************************
+ * Item list:
+ * 
+ * - 1 x Adafruit Feather
+ * - 1 x Micro USB cable
+ * - 1 x Protoboard
+ * - 1 x Led
+ * - 2 x 300ohm resistors
+ * - 7 x Jumper cables M/M
+ *******************************************************************************
+ * @author: Flavio H. C. Mauro
+ * @date:   06-Jan-2017
+ * LinkedIn: https://br.linkedin.com/in/flaviomauro
+ *******************************************************************************
+ */
+ 
+const int LED_PIN = 13; // Led connected to GPIO 13/D7
+const int BTN_PIN = 12; // Button connected to GPIO 12/D6
+int btnPressed = 0; // We'll use "btnPressed" to read the button state
+
+void setup(){
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BTN_PIN, INPUT);
+}
+
+void loop(){
+  btnPressed = digitalRead(BTN_PIN);
+  
+  if (btnPressed == HIGH){ // Button being pressed
+    digitalWrite(LED_PIN, HIGH); // Turn led on
+  }
+  else{ // Button is not being pressed
+    digitalWrite(LED_PIN, LOW); // Turn led off
+  }
+}
